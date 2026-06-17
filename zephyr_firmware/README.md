20c6e0e17b301a84923ebbc4df9dc5b9bc2b60bb
Eneji Ohieku <enejiohieku@gmail.com>
2026-06-17 09:01:18 +0000

    fixed compilation for esp32 and created Hardware component header

--- zephyr_firmware/README.md
+++ zephyr_firmware/README.md
@@ -6,3 +6,18 @@ The embedded client library for Zephyr RTOS that provides a high-level API for R
 - **Agent-less**: Communicates directly with ROS 2 via `zenoh`.
 - **ROS-like API**: Familiar `read` and `write` paradigms for embedded developers.
 - **Optimized**: Designed for low-memory footprint and real-time execution within the Zephyr thread model.
+
+## Setup instructions
+- Add zenoh-pico to workspace submanifest
+```bash
+cp zephyr_firmware/submanifests/zenoh-pico.yml /zephyr_ws/zephyr/submanifests/zenoh-pico.yml
+west update
+```
+
+- build
+
+```bash
+cd zephyr_firmware
+west build -p always -b esp32s3_devkitc/esp32s3/procpu #your appropriate board
+west flash --esp-device /dev/ttyUSB0
+```
--- zephyr_firmware/src/main.cpp
+++ zephyr_firmware/src/main.cpp
@@ -1,21 +1,18 @@
-// Copyright 2026 Open Source Robotics Foundation, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+#include <zenoh-pico.h>
+#include <zephyr/kernel.h>
+#include <zephyr/posix/pthread.h>
+#include <chrono>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <string_view>
 
-#include <iostream>
-
-int main(void)
+int main()
 {
-  std::cout << "Hello, C++ world! " << std::endl;
+  printk("\n=============================\n");
+  printk("  Hello, World! (Zephyr C++17)\n");
+  printk("  Board : %s\n", CONFIG_BOARD);
+  printk("=============================\n\n");
+
   return 0;
 }
