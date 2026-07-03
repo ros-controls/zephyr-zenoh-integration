d5b3c2cee15e47a49a3d77bf573ac376372dc89f
ShahazadAbdulla <Shahazad.abdulla.engineer@gmail.com>
2026-07-03 22:55:56 +0530

    feat: TIER1 CDR for JointState and JointCommand

--- demos/zenbedded_test_node/src/main.cpp
+++ demos/zenbedded_test_node/src/main.cpp
@@ -12,21 +12,110 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <zenoh-pico.h>
+#include <stdint.h>
+#include <stdio.h>
+
 #include <zephyr/kernel.h>
-#include <zephyr/posix/pthread.h>
-#include <chrono>
-#include <cstdio>
-#include <cstring>
-#include <string>
-#include <string_view>
+
+#include "zenbedded_transport/serialization.h"
+
+#ifdef CONFIG_TIER_1
+static void print_hex_dump(const char * label, const uint8_t * buffer, size_t size)
+{
+  printf("\n--- %s HEX DUMP (%zu bytes) ---\n", label, size);
+  for (size_t i = 0; i < size; i++)
+  {
+    printf("%02X ", buffer[i]);
+    if ((i + 1) % 16 == 0)
+    {
+      printf("\n");
+    }
+  }
+  printf("\n---------------------------------------\n");
+}
+#endif
 
 int main()
 {
-  printk("\n=============================\n");
-  printk("  Hello, World! (Zephyr C++17)\n");
-  printk("  Board : %s\n", CONFIG_BOARD);
-  printk("=============================\n\n");
+  printf("\n=======================================\n");
+  printf("  Zenbedded CDR: Integration Test Node\n");
+  printf("=======================================\n");
+
+#ifdef CONFIG_TIER_1
+  printf("\n[Tier 1 Native CDR Engine Enabled]\n");
+
+  // --- JointState Loopback Test ---
+  zcdr_joint_state_ctx_t state_ctx;
+  uint8_t state_buffer[512] = {0};
+
+  const char * frame_id = "base_link";
+  const char * joints[] = {"stepper", "pendulum"};
+  uint32_t num_joints = 2;
+
+  zcdr_init_joint_state(&state_ctx, frame_id, joints, num_joints, state_buffer);
+
+  double tx_pos[] = {1.57, -3.14};
+  double tx_vel[] = {0.5, 0.0};
+  double tx_eff[] = {1.2, 0.0};
+  zcdr_serialize_joint_state(&state_ctx, 1700000000, 500000, tx_pos, tx_vel, tx_eff, state_buffer);
+
+  printf("\n=== JointState Loopback ===\n");
+  print_hex_dump("JointState", state_buffer, state_ctx.payload_size);
+
+  int32_t rx_sec = 0;
+  uint32_t rx_nanosec = 0;
+  double rx_pos[2] = {0};
+  double rx_vel[2] = {0};
+  double rx_eff[2] = {0};
+
+  bool state_success = zcdr_deserialize_joint_state(
+    &state_ctx, state_buffer, state_ctx.payload_size, &rx_sec, &rx_nanosec, rx_pos, rx_vel, rx_eff);
+
+  if (state_success)
+  {
+    printf("SUCCESS: JointState payload extracted cleanly.\n");
+    printf("  -> Time: %d.%d\n", rx_sec, rx_nanosec);
+    printf("  -> Positions: [%f, %f]\n", rx_pos[0], rx_pos[1]);
+  }
+  else
+  {
+    printf("FAILED: Corrupted JointState payload.\n");
+  }
+
+  // --- JointCommand Loopback Test ---
+  zcdr_joint_command_ctx_t cmd_ctx;
+  uint8_t cmd_buffer[512] = {0};
+  const char * interface_name = "velocity";
+
+  zcdr_init_joint_command(&cmd_ctx, frame_id, joints, num_joints, interface_name, cmd_buffer);
+
+  double cmd_tx_values[] = {10.5, -5.25};
+  zcdr_serialize_joint_command(&cmd_ctx, 1700000001, 600000, cmd_tx_values, cmd_buffer);
+
+  printf("\n=== JointCommand Loopback ===\n");
+  print_hex_dump("JointCommand", cmd_buffer, cmd_ctx.payload_size);
+
+  int32_t cmd_rx_sec = 0;
+  uint32_t cmd_rx_nano = 0;
+  double cmd_rx_values[2] = {0};
+
+  bool cmd_success = zcdr_deserialize_joint_command(
+    &cmd_ctx, cmd_buffer, cmd_ctx.payload_size, &cmd_rx_sec, &cmd_rx_nano, cmd_rx_values);
+
+  if (cmd_success)
+  {
+    printf("SUCCESS: JointCommand payload extracted cleanly.\n");
+    printf("  -> Time: %d.%d\n", cmd_rx_sec, cmd_rx_nano);
+    printf("  -> Values: [%f, %f]\n", cmd_rx_values[0], cmd_rx_values[1]);
+  }
+  else
+  {
+    printf("FAILED: Corrupted JointCommand payload.\n");
+  }
+
+#else
+  printf("\nCONFIG_TIER_1 is disabled. Skipping CDR tests.\n");
+#endif
 
   return 0;
 }
--- zenbedded_transport/src/serialization.cpp
+++ zenbedded_transport/src/serialization.cpp
@@ -1,4 +1,4 @@
-// Copyright 2026
+// Copyright 2026 Zenbedded
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,4 +12,249 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "zenbedded_transport/serialization.h"  // NOLINT(build/include_subdir)
+#include "zenbedded_transport/serialization.h"
+
+#include <string.h>
+
+#ifdef CONFIG_TIER_1
+
+#define ALIGN_UP(offset, alignment) (((offset) + (alignment) - 1) & ~((alignment) - 1))
+
+void zcdr_init_joint_state(
+  zcdr_joint_state_ctx_t * ctx, const char * frame_id, const char ** joint_names,
+  uint32_t num_joints, uint8_t * out_buffer)
+{
+  ctx->num_joints = num_joints;
+  size_t cursor = 0;
+
+  out_buffer[0] = 0x00;
+  out_buffer[1] = 0x01;
+  out_buffer[2] = 0x00;
+  out_buffer[3] = 0x00;
+  cursor += 4;
+
+  ctx->offset_timestamp = cursor;
+  cursor += 8;
+
+  uint32_t frame_id_len = frame_id ? strlen(frame_id) + 1 : 1;
+  memcpy(&out_buffer[cursor], &frame_id_len, 4);
+  cursor += 4;
+
+  if (frame_id)
+  {
+    memcpy(&out_buffer[cursor], frame_id, frame_id_len);
+  }
+  else
+  {
+    out_buffer[cursor] = '\0';
+  }
+  cursor += frame_id_len;
+
+  cursor = ALIGN_UP(cursor, 4);
+  memcpy(&out_buffer[cursor], &num_joints, 4);
+  cursor += 4;
+
+  for (uint32_t i = 0; i < num_joints; i++)
+  {
+    uint32_t name_len = strlen(joint_names[i]) + 1;
+    cursor = ALIGN_UP(cursor, 4);
+    memcpy(&out_buffer[cursor], &name_len, 4);
+    cursor += 4;
+    memcpy(&out_buffer[cursor], joint_names[i], name_len);
+    cursor += name_len;
+  }
+
+  cursor = ALIGN_UP(cursor, 4);
+  memcpy(&out_buffer[cursor], &num_joints, 4);
+  cursor += 4;
+  cursor = ALIGN_UP(cursor, 8);
+  ctx->offset_position = cursor;
+  cursor += (8 * num_joints);
+
+  cursor = ALIGN_UP(cursor, 4);
+  memcpy(&out_buffer[cursor], &num_joints, 4);
+  cursor += 4;
+  cursor = ALIGN_UP(cursor, 8);
+  ctx->offset_velocity = cursor;
+  cursor += (8 * num_joints);
+
+  cursor = ALIGN_UP(cursor, 4);
+  memcpy(&out_buffer[cursor], &num_joints, 4);
+  cursor += 4;
+  cursor = ALIGN_UP(cursor, 8);
+  ctx->offset_effort = cursor;
+  cursor += (8 * num_joints);
+
+  ctx->payload_size = cursor;
+}
+
+void zcdr_serialize_joint_state(
+  const zcdr_joint_state_ctx_t * ctx, int32_t stamp_sec, uint32_t stamp_nanosec,
+  const double * positions, const double * velocities, const double * efforts, uint8_t * out_buffer)
+{
+  memcpy(&out_buffer[ctx->offset_timestamp], &stamp_sec, 4);
+  memcpy(&out_buffer[ctx->offset_timestamp + 4], &stamp_nanosec, 4);
+
+  size_t array_bytes = ctx->num_joints * 8;
+
+  if (positions)
+  {
+    memcpy(&out_buffer[ctx->offset_position], positions, array_bytes);
+  }
+  if (velocities)
+  {
+    memcpy(&out_buffer[ctx->offset_velocity], velocities, array_bytes);
+  }
+  if (efforts)
+  {
+    memcpy(&out_buffer[ctx->offset_effort], efforts, array_bytes);
+  }
+}
+
+bool zcdr_deserialize_joint_state(
+  const zcdr_joint_state_ctx_t * ctx, const uint8_t * in_buffer, size_t buffer_size,
+  int32_t * out_stamp_sec, uint32_t * out_stamp_nanosec, double * out_positions,
+  double * out_velocities, double * out_efforts)
+{
+  if (buffer_size < ctx->payload_size)
+  {
+    return false;
+  }
+
+  if (out_stamp_sec)
+  {
+    memcpy(out_stamp_sec, &in_buffer[ctx->offset_timestamp], 4);
+  }
+  if (out_stamp_nanosec)
+  {
+    memcpy(out_stamp_nanosec, &in_buffer[ctx->offset_timestamp + 4], 4);
+  }
+
+  size_t array_bytes = ctx->num_joints * 8;
+
+  if (out_positions)
+  {
+    memcpy(out_positions, &in_buffer[ctx->offset_position], array_bytes);
+  }
+  if (out_velocities)
+  {
+    memcpy(out_velocities, &in_buffer[ctx->offset_velocity], array_bytes);
+  }
+  if (out_efforts)
+  {
+    memcpy(out_efforts, &in_buffer[ctx->offset_effort], array_bytes);
+  }
+
+  return true;
+}
+
+void zcdr_init_joint_command(
+  zcdr_joint_command_ctx_t * ctx, const char * frame_id, const char ** joint_names,
+  uint32_t num_joints, const char * interface_name, uint8_t * out_buffer)
+{
+  ctx->num_joints = num_joints;
+  size_t cursor = 0;
+
+  out_buffer[0] = 0x00;
+  out_buffer[1] = 0x01;
+  out_buffer[2] = 0x00;
+  out_buffer[3] = 0x00;
+  cursor += 4;
+
+  ctx->offset_timestamp = cursor;
+  cursor += 8;
+
+  uint32_t frame_id_len = frame_id ? strlen(frame_id) + 1 : 1;
+  memcpy(&out_buffer[cursor], &frame_id_len, 4);
+  cursor += 4;
+
+  if (frame_id)
+  {
+    memcpy(&out_buffer[cursor], frame_id, frame_id_len);
+  }
+  else
+  {
+    out_buffer[cursor] = '\0';
+  }
+  cursor += frame_id_len;
+
+  cursor = ALIGN_UP(cursor, 4);
+  memcpy(&out_buffer[cursor], &num_joints, 4);
+  cursor += 4;
+
+  for (uint32_t i = 0; i < num_joints; i++)
+  {
+    uint32_t name_len = strlen(joint_names[i]) + 1;
+    cursor = ALIGN_UP(cursor, 4);
+    memcpy(&out_buffer[cursor], &name_len, 4);
+    cursor += 4;
+    memcpy(&out_buffer[cursor], joint_names[i], name_len);
+    cursor += name_len;
+  }
+
+  uint32_t interface_len = interface_name ? strlen(interface_name) + 1 : 1;
+  cursor = ALIGN_UP(cursor, 4);
+  memcpy(&out_buffer[cursor], &interface_len, 4);
+  cursor += 4;
+
+  if (interface_name)
+  {
+    memcpy(&out_buffer[cursor], interface_name, interface_len);
+  }
+  else
+  {
+    out_buffer[cursor] = '\0';
+  }
+  cursor += interface_len;
+
+  cursor = ALIGN_UP(cursor, 4);
+  memcpy(&out_buffer[cursor], &num_joints, 4);
+  cursor += 4;
+
+  cursor = ALIGN_UP(cursor, 8);
+  ctx->offset_values = cursor;
+  cursor += (8 * num_joints);
+
+  ctx->payload_size = cursor;
+}
+
+void zcdr_serialize_joint_command(
+  const zcdr_joint_command_ctx_t * ctx, int32_t stamp_sec, uint32_t stamp_nanosec,
+  const double * values, uint8_t * out_buffer)
+{
+  memcpy(&out_buffer[ctx->offset_timestamp], &stamp_sec, 4);
+  memcpy(&out_buffer[ctx->offset_timestamp + 4], &stamp_nanosec, 4);
+
+  if (values)
+  {
+    memcpy(&out_buffer[ctx->offset_values], values, ctx->num_joints * 8);
+  }
+}
+
+bool zcdr_deserialize_joint_command(
+  const zcdr_joint_command_ctx_t * ctx, const uint8_t * in_buffer, size_t buffer_size,
+  int32_t * out_stamp_sec, uint32_t * out_stamp_nanosec, double * out_values)
+{
+  if (buffer_size < ctx->payload_size)
+  {
+    return false;
+  }
+
+  if (out_stamp_sec)
+  {
+    memcpy(out_stamp_sec, &in_buffer[ctx->offset_timestamp], 4);
+  }
+  if (out_stamp_nanosec)
+  {
+    memcpy(out_stamp_nanosec, &in_buffer[ctx->offset_timestamp + 4], 4);
+  }
+
+  if (out_values)
+  {
+    memcpy(out_values, &in_buffer[ctx->offset_values], ctx->num_joints * 8);
+  }
+
+  return true;
+}
+
+#endif  // CONFIG_TIER_1
