743d53701c6ba0f5ecfe02fa2de784597485c7c8
Eneji Ohieku <enejiohieku@gmail.com>
2026-07-26 18:49:27 +0100

    Feat: Add sine-wave demo, ZenbeddedHardware + Zephyr firmware end-to-end example

---
+++ demos/sine_wave/sine_wave_ros/launch/sine_wave.launch.py
@@ -0,0 +1,58 @@
+# Copyright 2026 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import xacro
+from launch import LaunchDescription
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessStart
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.parameter_descriptions import ParameterValue
+
+
+def generate_launch_description():
+    pkg_share = get_package_share_directory("zenbedded_sine_wave")
+
+    robot_description_content = xacro.process_file(
+        os.path.join(pkg_share, "urdf", "sine_wave.urdf"),
+    ).toxml()
+
+    robot_description = {
+        "robot_description": ParameterValue(robot_description_content, value_type=str)
+    }
+    controllers_yaml = os.path.join(pkg_share, "config", "controllers.yaml")
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="screen",
+        parameters=[robot_description],
+    )
+
+    ros2_control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[controllers_yaml],
+        output="screen",
+    )
+
+    delay_ros2_control_after_rsp = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=robot_state_publisher_node,
+            on_start=[ros2_control_node],
+        )
+    )
+
+    return LaunchDescription([robot_state_publisher_node, delay_ros2_control_after_rsp])
