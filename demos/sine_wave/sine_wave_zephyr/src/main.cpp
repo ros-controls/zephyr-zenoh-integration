acae77be86ffc58968380a23833b16b612a1b77f
Eneji Ohieku <enejiohieku@gmail.com>
2026-08-11 10:29:25 +0100

    Used the wifi credentials library

--- demos/sine_wave/sine_wave_zephyr/src/main.cpp
+++ demos/sine_wave/sine_wave_zephyr/src/main.cpp
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <esp_wifi.h>
 #include <zephyr/kernel.h>
-#include <zephyr/logging/log.h>
 #include <zephyr/net/net_event.h>
 #include <zephyr/net/net_if.h>
 #include <zephyr/net/wifi_mgmt.h>
@@ -22,17 +22,11 @@
 
 LOG_MODULE_REGISTER(sine_wave_zephyr, LOG_LEVEL_INF);
 
-#ifndef WIFI_SSID
-#define WIFI_SSID "mechatronic"
-#endif
-#ifndef WIFI_PSK
-#define WIFI_PSK "87654321"
-#endif
 #ifndef ZENOH_MODE
 #define ZENOH_MODE "client"
 #endif
 #ifndef ZENOH_LOCATOR
-#define ZENOH_LOCATOR "tcp/10.70.246.128:7447"
+#define ZENOH_LOCATOR "tcp/10.75.105.128:7447"
 #endif
 
 #define STATE_TOPIC "zenbedded/sine_wave/state"
@@ -40,42 +34,13 @@ LOG_MODULE_REGISTER(sine_wave_zephyr, LOG_LEVEL_INF);
 
 K_SEM_DEFINE(network_connected_sem, 0, 1);
 
-void net_event_handler(net_mgmt_event_callback * cb, uint32_t mgmt_event, net_if * iface)
+void net_event_handler(net_mgmt_event_callback * cb, uint64_t mgmt_event, net_if * iface)
 {
   if (mgmt_event == NET_EVENT_IPV4_ADDR_ADD)
   {
     LOG_INF("got IPv4 address");
     k_sem_give(&network_connected_sem);
   }
-}
-
-int connect_wifi_blocking()
-{
-  static net_mgmt_event_callback cb;
-  net_mgmt_init_event_callback(&cb, net_event_handler, NET_EVENT_IPV4_ADDR_ADD);
-  net_mgmt_add_event_callback(&cb);
-
-  net_if * iface = net_if_get_default();
-
-  wifi_connect_req_params params = {
-    .ssid = reinterpret_cast<const uint8_t *>(WIFI_SSID),
-    .ssid_length = sizeof(WIFI_SSID) - 1,
-    .psk = reinterpret_cast<const uint8_t *>(WIFI_PSK),
-    .psk_length = sizeof(WIFI_PSK) - 1,
-    .channel = WIFI_CHANNEL_ANY,
-    .security = WIFI_SECURITY_TYPE_PSK,
-  };
-
-  LOG_INF("connecting to WiFi...");
-  int ret = net_mgmt(NET_REQUEST_WIFI_CONNECT, iface, &params, sizeof(params));
-  if (ret)
-  {
-    LOG_ERR("WiFi connect request failed: %d", ret);
-    return ret;
-  }
-
-  k_sem_take(&network_connected_sem, K_FOREVER);
-  return 0;
 }
 
 float wave_amp = 5;
@@ -85,9 +50,13 @@ uint32_t loop_freq = 100;    // hz
 int main()
 {
   LOG_INF("starting test node");
-  k_sleep(K_MSEC(2000));
-  connect_wifi_blocking();
-  k_sleep(K_MSEC(100));
+
+  LOG_INF("Waiting for IPV4 address");
+  k_sem_take(&network_connected_sem, K_FOREVER);
+
+  // Disable Wi-Fi power saving
+  esp_wifi_set_ps(WIFI_PS_NONE);
+  k_sleep(K_MSEC(200));
 
   static ZenbeddedClient client;
 
@@ -106,7 +75,7 @@ int main()
     const zenbedded_command_t & cmd = client.command();
     zenbedded_state_t & state = client.state();
 
-    if (cmd.sine_wave_amplitude != 0.0f)
+    if (fabsf(cmd.sine_wave_amplitude) >= 1e-6)
     {
       cmd_recv = true;
     }
@@ -115,9 +84,9 @@ int main()
       wave_amp = fabsf(cmd.sine_wave_amplitude);
     }
 
-    auto current_time_ms = static_cast<float>(k_uptime_get() - start_time);
-    float x = 2 * static_cast<float>(M_PI) * current_time_ms * wave_frequency / 1000.0f;
-    state.sine_wave_position = wave_amp * sinf(x);
+    auto current_time_ms = static_cast<double>(k_uptime_get() - start_time);
+    double x = 2.0 * M_PI * current_time_ms * wave_frequency / 1000.0;
+    state.sine_wave_position = wave_amp * sin(x);
 
     if (++cnt == 500)
     {
